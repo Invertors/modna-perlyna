e1ebf3250e0013ac641debfb53e7505cdfbd63da
compl1cted <mikigriks@gmail.com>
2024-03-19 14:44:12 +0200

    Push attempt

---
+++ bot/src/index.js
@@ -0,0 +1,38 @@
+const { Telegraf } = require('telegraf');
+const fastify = require('fastify');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.register('Welcome'));
+bot.launch().catch(console.error);
+
+const app = fastify();
+const port = process.env.PORT || 9011;
+const channelId = process.env.CHANNEL_TOKEN;
+
+const formatMessage = (model, color, size, customerName, phoneNumber) => {
+    const date = new Date();
+    const formatDate = `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`
+    return `*Замовлення:*\n
+    Модель: *${model}*,
+    Колір: *${color}*,
+    Розмір: *${size}*,
+    Ім'я Замовника: *${customerName}*,
+    Номер Телефону: *${phoneNumber}*,
+    Дата замовлення: *${formatDate}*`
+}
+
+app.post('/bot/', (req, reply) => {
+    console.log(req.body);
+    const { model, color, size, customerName, phoneNumber } = req.body;
+    const message = formatMessage(model, color, size, customerName, phoneNumber);
+    bot.telegram.sendMessage(channelId, message, {parse_mode: 'Markdown'}).catch(console.error);
+    reply.send({text: 'Order submitted!'});
+});
+
+app.listen({port}, (err) => {
+    if (err) {
+        console.error(`Error occurred while staring the bot: ${err}`);
+        return;
+    }
+    console.log(`Server started on port: ${port}`);
+});
---
+++ static/walking_suit/js/form_handler.js
@@ -0,0 +1,65 @@
+const form = document.getElementById('order_form');
+const userName = document.getElementById('userName');
+const phoneNumber = document.getElementById('phoneNumber');
+const color = document.getElementById('color');
+const size = document.getElementById('size');
+const model = document.getElementById('model');
+phoneNumber.oninput = function(){
+    this.value = this.value.replace(/[^0-9\+]/g, '');
+}
+
+const sendForm = async ()=>{
+    try{
+
+        await fetch('https://modnapearl.shop/bot/',{
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify({
+                [userName.name]: userName.value.trim(),
+                [phoneNumber.name]: phoneNumber.value.trim(),
+                [color.name]: color.value.trim(),
+                [size.name]:size.value,
+		        [model.name]:model.value,
+            })
+        }).then(response =>{
+                if (response.status >= 200 && response.status < 300){
+                    form.reset();
+		            alert("Ми отримали замовлення та з'вяжимося з вами найближчим часом!")
+                }else{
+                    alert('Помилка');
+                }
+            }
+        ).catch(()=>{
+            alert('Помилка');
+        });
+    }catch(ex){
+        console.error(ex);
+    }
+}
+
+
+form.onsubmit = (event) => {
+        event.preventDefault();
+        let phoneVal = phoneNumber.value.trim();
+        let colorVal = color.value;
+        let sizeVal = size.value;
+
+        if (colorVal === "Оберіть колір"){
+            alert("Оберіть колір");
+            return false;
+        }
+        if (sizeVal === "Оберіть розмір"){
+            alert("Оберіть розмір");
+            return false;
+        }
+        if (phoneVal.indexOf('_') !== -1 || phoneVal == null || typeof phoneVal === 'undefined' || phoneVal == '') {
+            alert('Введіть ваш номер телефона!');
+            return false;
+        }
+        sendForm();
+
+    return false;
+}
